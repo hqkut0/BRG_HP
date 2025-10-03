ad3b855bfd84968873a591e3dad4e163fe0c4135
hqkut0 <manami19960517@gmail.com>
2025-10-03 01:56:51 +0000

    運営のメンバー紹介修正

---
+++ main/src/memberProfile/manager/manager.js
@@ -0,0 +1,21 @@
+// マネージャー情報を配列で定義
+const STREAMERS = [
+  {
+    name: "黒岩 白兎",
+    img: "hakuto_2.png",
+    large: "hakuto.png",
+    youtube: "https://www.youtube.com/@hakuto_snowlove",
+    twitch: "https://www.twitch.tv/hakuto_kuro_vt",
+    x: "https://x.com/hakuto_snowlove",
+    highlight: "https://www.youtube.com/watch?v=SO8Yd0D3GXo"
+  },
+  {
+    name: "Kenken",
+    img: "kenken.png",
+    large: "kenken.png",
+    youtube: "https://www.youtube.com/@ynkskenken2216",
+    twitch: "https://www.twitch.tv/kenken_signal",
+    x: "https://x.com/IgrKenken",
+    highlight: "https://www.youtube.com/watch?v=2NxlkTPh3W8"
+  }
+];
