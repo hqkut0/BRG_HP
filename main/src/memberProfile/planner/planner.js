d0b6d56b6496b4a744b6cf7cc34c0647e6943432
hqkut0 <manami19960517@gmail.com>
2025-10-03 05:09:30 +0000

    企画部門のメンバー紹介修正

---
+++ main/src/memberProfile/planner/planner.js
@@ -0,0 +1,55 @@
+// プランナー情報を配列で定義
+const PLANNERS = [
+  {
+    name: "松下武瑠",
+    img: "takeru.png",
+    large: "takeru.png",
+    youtube: "https://www.youtube.com/@Takeru_Matsusita",
+    x: "https://x.com/M_Takeru0505",
+    highlight: "https://www.youtube.com/watch?v=W-RIzmOjh1I"
+  },
+    {
+    name: "龍黒野",
+    img: "ryukurono.png",
+    large: "ryukurono.png",
+    youtube: "https://www.youtube.com/@Takeru_Matsusita",
+    x: "https://x.com/M_Takeru0505",
+    highlight: "https://www.youtube.com/watch?v=W-RIzmOjh1I"
+  },
+  {
+    name: "フォリウム",
+    img: "forium.png",
+    large: "forium.png",
+    youtube: "https://www.youtube.com/@Takeru_Matsusita",
+    x: "https://x.com/M_Takeru0505",
+    highlight: "https://www.youtube.com/watch?v=W-RIzmOjh1I"
+  },
+  {
+    name: "白銀琉杏",
+    img: "ruan_2.png",
+    large: "ruan.png",
+    youtube: "https://www.youtube.com/@Takeru_Matsusita",
+    x: "https://x.com/M_Takeru0505",
+    highlight: "https://www.youtube.com/watch?v=W-RIzmOjh1I"
+  },
+  {
+    name: "じゅにあ",
+    img: "junior.png",
+    large: "junior.png",
+    youtube: "https://www.youtube.com/@Takeru_Matsusita",
+    x: "https://x.com/M_Takeru0505",
+    highlight: "https://www.youtube.com/watch?v=W-RIzmOjh1I"
+  },
+  {
+    name: "雀野琴香",
+    img: "kotoka.png",
+    large: "kotoka.png",
+    youtube: "https://www.youtube.com/@Takeru_Matsusita",
+    x: "https://x.com/M_Takeru0505",
+    highlight: "https://www.youtube.com/watch?v=W-RIzmOjh1I"
+  },
+
+];
+
+// ここでグローバルのROLE_PLANNERを上書き
+window.ROLE_PLANNER = PLANNERS;
